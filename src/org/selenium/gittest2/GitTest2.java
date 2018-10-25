028fe62dc3771752fb265e66d764065140633e95
aruns87 <arunkumar.grand@gmail.com>
2018-10-25 10:58:47 +0530

    Main Code

---
+++ src/org/selenium/gittest2/GitTest2.java
@@ -0,0 +1,25 @@
+package org.selenium.gittest2;
+
+public class GitTest2 {
+	public static void main(String[] args) {
+
+		int a[]= new int[10];
+		int sm=0;
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+		a[5]=6;
+		a[6]=7;
+		a[7]=8;
+		a[8]=9;
+		a[9]=10;
+
+		for(int i=0;i<a.length;i++)
+		{
+			sm=sm+a[i];
+		}
+		System.out.println("Sum of array values :" +sm);
+		}
+}
